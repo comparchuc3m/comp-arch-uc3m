196d915c4f116491a6fbe173a887c7d537711af1
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-10-12 12:45:55 +0200

    [en]Exercise m4-01

--- en/ex/Makefile.mk
+++ en/ex/Makefile.mk
@@ -2,3 +2,4 @@ include $(EN_EX_M2_01_DIR)/Makefile.mk
 include $(EN_EX_M2_02_DIR)/Makefile.mk
 include $(EN_EX_M3_01_DIR)/Makefile.mk
 include $(EN_EX_M3_02_DIR)/Makefile.mk
+include $(EN_EX_M4_01_DIR)/Makefile.mk
