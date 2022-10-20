73a057ac2f5d2927db57bd4906e0b6c9a19bf72e
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-10-20 16:07:32 +0200

    m6-02: language typo

--- int/m6-02-cppconc/race.cpp
+++ int/m6-02-cppconc/race.cpp
@@ -5,7 +5,7 @@ void g() { x=0; }
 void h() { std::cout << "C++" << "\n"; }
 void i() { std::cout << "Python" << "\n"; }
 
-void carrera() {
+void data_races() {
   std::thread t1{f}; std::thread t2{g};
   t1.join(); t2.join();
 
