c836c6da2b5e531fa628acb5fa7df3fcd71aca1f
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-10-23 10:38:10 +0200

    Removed lang dependent comments

--- int/m6-03-omp/examples/firstprivate/main.cpp
+++ int/m6-03-omp/examples/firstprivate/main.cpp
@@ -6,8 +6,8 @@ constexpr long maxval = 10;//10000000;
 void f() {
   int x = 17;
   #pragma omp parallel for firstprivate(x) 
-  for (long i=0;i<maxval;++i) {
-    x += i;  // x inicialmente vale 17
+  for (long i=0;i<maxval;++i) { // x==17
+    x += i;  
   }
   std::cout << x << '\n'; // x==17
 }
--- int/m6-03-omp/examples/lastprivate/main.cpp
+++ int/m6-03-omp/examples/lastprivate/main.cpp
@@ -6,10 +6,10 @@ constexpr long maxval = 10;//10000000;
 void f() {
   int x = 17;
   #pragma omp parallel for firstprivate(x) lastprivate(x) 
-  for (long i=0;i<maxval;++i) {
-    x += i;  // x inicialmente vale 17
+  for (long i=0;i<maxval;++i) { // x==17
+    x += i;  
   }
-  std::cout << x << '\n'; // x valor iteración i==maxval-1
+  std::cout << x << '\n'; // x == 17 + sum(0..maxval-1)
 }
 
 int main() {
