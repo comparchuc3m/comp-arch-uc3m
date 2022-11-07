036538ef9531d373a724b267ced02af2f65b5f28
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-11-06 16:39:55 -1000

    proj-02: Project statement

--- es/proj/Makefile.mk
+++ es/proj/Makefile.mk
@@ -1,1 +1,2 @@
 include $(ES_PROJ_IMG_01_DIR)/Makefile.mk
+include $(ES_PROJ_IMG_02_DIR)/Makefile.mk
---
+++ es/proj/img-02/Makefile.mk
@@ -0,0 +1,2 @@
+$(FINAL_ES_PROJ_IMG_02_PDF): $(ES_PROJ_IMG_02_PARTS) $(ES_CONFIG_INPUT)
+	$(LATEXMK) -jobname=$(subst /,-,$(<D)) $(ES_PROJ_IMG_02_DIR)/ex
