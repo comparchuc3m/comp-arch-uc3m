426c4b7f5c64375075abc057153d2a6f4d33cadf
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-11-09 16:37:48 -1000

    m5-02: Memory consistency

---
+++ es/m5-02-consist/Makefile.mk
@@ -0,0 +1,5 @@
+$(FINAL_ES_M5_02_CONSIST_LECT_PDF): $(ES_M5_02_CONSIST_PARTS) $(ES_CONFIG_INPUT)
+	$(LATEXMK) -jobname=$(subst /,-,$(<D))-lect $(ES_M5_02_CONSIST_DIR)/slides-lect
+
+$(FINAL_ES_M5_02_CONSIST_HOUT_PDF): $(ES_M5_02_CONSIST_PARTS) $(ES_CONFIG_INPUT)
+	$(LATEXMK) -jobname=$(subst /,-,$(<D))-hout $(ES_M5_02_CONSIST_DIR)/slides-hout
