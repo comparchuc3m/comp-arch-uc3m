c35171934343a3ced91ea4ad7a823eefad9fee72
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-11-10 15:57:05 -1000

    m5-??-consist: Renamed to m5-03-consist

--- en/m5-02-consist/Makefile.mk
+++
@@ -1,5 +0,0 @@
-$(FINAL_EN_M5_02_CONSIST_LECT_PDF): $(EN_M5_02_CONSIST_PARTS) $(EN_CONFIG_INPUT)
-	$(LATEXMK) -jobname=$(subst /,-,$(<D))-lect $(EN_M5_02_CONSIST_DIR)/slides-lect
-
-$(FINAL_EN_M5_02_CONSIST_HOUT_PDF): $(EN_M5_02_CONSIST_PARTS) $(EN_CONFIG_INPUT)
-	$(LATEXMK) -jobname=$(subst /,-,$(<D))-hout $(EN_M5_02_CONSIST_DIR)/slides-hout
