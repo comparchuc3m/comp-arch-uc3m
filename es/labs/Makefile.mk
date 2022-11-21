e51279108fdd828ff8e92ec867dd08271cfe402f
J. Daniel Garcia <josedaniel.garcia@uc3m.es>
2022-11-21 23:19:42 +0100

    [es] lab-06

--- es/labs/Makefile.mk
+++ es/labs/Makefile.mk
@@ -2,3 +2,4 @@ include $(ES_LAB1_CPPDEV_DIR)/Makefile.mk
 include $(ES_LAB2_PERFENER_DIR)/Makefile.mk
 include $(ES_LAB3_CACHE_DIR)/Makefile.mk
 include $(ES_LAB5_ILP_DIR)/Makefile.mk
+include $(ES_LAB6_ATOMICS_DIR)/Makefile.mk
