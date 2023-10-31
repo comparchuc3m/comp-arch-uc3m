75410b206800934a4bd09634b36cad7c05802ab0
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2023-10-31 10:11:12 +0100

    OpenMP Lab5

--- en/labs/Makefile.mk
+++ en/labs/Makefile.mk
@@ -1,5 +1,6 @@
 include $(EN_LAB1_CPPDEV_DIR)/Makefile.mk
 include $(EN_LAB2_PERFENER_DIR)/Makefile.mk
 include $(EN_LAB3_CACHE_DIR)/Makefile.mk
+include $(EN_LAB5_OMP_DIR)/Makefile.mk
 include $(EN_LAB5_ILP_DIR)/Makefile.mk
 include $(EN_LAB6_ATOMICS_DIR)/Makefile.mk
