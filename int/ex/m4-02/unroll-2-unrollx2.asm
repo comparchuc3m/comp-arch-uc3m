c9b943f5808c4a4534bc1e3637e83fbf9709d923
Alejandro Calderon <acaldero.uc3m@gmail.com>
2023-11-01 15:48:07 +0100

    Update unroll-2-unrollx2.asm

--- int/ex/m4-02/unroll-2-unrollx2.asm
+++ int/ex/m4-02/unroll-2-unrollx2.asm
@@ -1,18 +1,18 @@
-loop: fld     ft0,  (t1)
-      fld     ft1,  (t2)
+loop: fld     ft0,  0(t1)
+      fld     ft1,  0(t2)
       fld     ft4,  8(t1)
       fld     ft5,  8(t2)
       fadd.d  ft2,  ft0,   ft1
-      addi    t1,   t1,    8
+      addi    t1,   t1,    16
       fadd.d  ft6,  ft4,   ft5
-      addi    t2,   t2,    8
-      subi    t4,   t4,    1
+      addi    t2,   t2,    16
+      subi    t4,   t4,    2
       fmul.d  ft2,  ft2,   ft3
       <stall> x 1
       fmul.d  ft6,  ft6,   ft3
       <stall> x 4
-      fsd     ft2,  (t3)
+      fsd     ft2,  0(t3)
       <stall> x 1
       fsd     ft6,  8(t3)
       bne     t4,   zero,  loop
-      addi    t3,   t3,    8
+      addi    t3,   t3,    16
