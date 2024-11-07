60412b8d0d1e940397638cc00c5a5f530f1a37ce
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2024-11-07 12:20:15 +0100

    Updated omp lab to avignon current setup

--- lab/05-omp/build.sh
+++ lab/05-omp/build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-
-. /etc/profile
-module avail
-module load gcc/12.1.0
-cmake --version
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build -j 8
--- lab/05-omp/run-gray-omp-stan.sh
+++ lab/05-omp/run-gray-omp-stan.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-
-. /etc/profile
-module avail
-module load gcc/12.1.0
 OMP_NUM_THREADS=1 perf stat build-stan/imgtool/imgtool par_grayscale in/sabatini.bmp sabatini-gpar.bmp
 OMP_NUM_THREADS=2 perf stat build-stan/imgtool/imgtool par_grayscale in/sabatini.bmp sabatini-gpar.bmp
 OMP_NUM_THREADS=4 perf stat build-stan/imgtool/imgtool par_grayscale in/sabatini.bmp sabatini-gpar.bmp
