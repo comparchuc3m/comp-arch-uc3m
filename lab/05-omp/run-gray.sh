9be4a22c595d935a792a3b4b0b556abb24b2b18e
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2024-11-07 12:20:52 +0100

    Updated omp lab to avignon current setup (#24)

--- lab/05-omp/run-gray.sh
+++ lab/05-omp/run-gray.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-. /etc/profile
-module avail
-module load gcc/12.1.0
 perf stat build/imgtool/imgtool grayscale in/sabatini.bmp sabatini-gseq.bmp
--- lab/05-omp/run-histo-stan.sh
+++ lab/05-omp/run-histo-stan.sh
@@ -1,6 +1,2 @@
-!/bin/sh
-
-. /etc/profile
-module avail
-module load gcc/12.1.0
+#!/bin/sh
 perf stat build-stan/imgtool/imgtool histogram in/sabatini.bmp sabatini-seq.hst
