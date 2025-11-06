70f45402e878bd0105582ea991cfaef3d8d0258f
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2025-11-07 00:27:46 +0100

    Mejora de slides sobre hilos

--- int/m6-02-cppconc/launch_lambda.cpp
+++ int/m6-02-cppconc/launch_lambda.cpp
@@ -2,14 +2,12 @@ void myfunc(image & img);
 
 void g() {
   image img = get_image();
-  std::thread t1{
+  std::jthread t1{
     [&] {
       fft(img);
       filter(img);
       inv_fft(img)
     }
   };
-
-  // Threads joins
-  // ...
+  //...
 }
